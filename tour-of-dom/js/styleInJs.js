c6d02ba5a228524eeb3570b45e40dd19208a12cd
sagorsenanupam <senanupam10@gmail.com>
2025-06-09 23:26:55 +0600

    edited

---
+++ tour-of-dom/js/styleInJs.js
@@ -0,0 +1,11 @@
+const sections = document.querySelectorAll('section');
+for (const section of sections) {
+    section.style.border = '2px solid black';
+    section.style.marginBottom = '10px';
+    section.style.borderRadius = '5px';
+    section.style.paddingLeft = '7px';
+    section.style.backgroundColor = 'lightblue';
+}
+
+const placesContainer = document.getElementById('places-container');
+placesContainer.style.backgroundColor = 'yellow';
